0da217c72bb287df2041c5cad603fe8a631d8640
stampanone <blackqueen_nyj@hotmail.com>
2023-03-25 23:15:53 -0300

    correccion de boton de editar

--- src/app/componentes/informacion/informacion.component.ts
+++ src/app/componentes/informacion/informacion.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Persona } from 'src/app/entidad/persona';
 import { DatosService } from 'src/app/service/datos.service';
 import { PersonaService } from 'src/app/service/persona.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-informacion',
@@ -11,12 +13,18 @@ import { PersonaService } from 'src/app/service/persona.service';
 export class InformacionComponent implements OnInit {
   acerca: string = "";
   Persona: any;
+  isLogged= false;
   
   
-  constructor(private servPers: PersonaService) { }
+  constructor(private servPers: PersonaService, private tokenService: TokenService, private router: Router) { }
 
   ngOnInit(): void {
     this.cargarPersona();
+    if (this.tokenService.getToken()){
+      this.isLogged= true;
+    }else{
+      this.isLogged= false;
+    }
   }
   cargarPersona():void{
     this.servPers.list().subscribe (data => {this.Persona=data});
